cff53ca760d0a851d8cbed5e32299c7e4849085b
Daniel Schmidt <dschmidt@weluse.de>
2014-12-23 17:31:56 +0100

    styled events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,11 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_event, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
   respond_to :html
 
   def index
     @events = current_user.events.all
     respond_with(@events)
-  end
-
-  def show
-    respond_with(@event)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events, except: [:show]
 
   devise_for :users
   root 'static#index'
