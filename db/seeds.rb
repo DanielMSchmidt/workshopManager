e9f94ad79970281590d93018349ea89edb2e2862
Daniel Schmidt <dschmidt@weluse.de>
2014-12-23 15:47:18 +0100

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+unless Rails.env.production?
+  user = User.create!(email: 'test@test.de', password: 'testtest', password_confirmation: 'testtest' )
+  event = Event.create!(user_id: user.id, name: 'Test Event', description: 'Testdescription', start: Time.now, duration: 1.5)
+  event_subscriber = Subscriber.create!(email: 'event_subscriber@test.de')
+  user_subscriber = Subscriber.create!(email: 'user_subscriber@test.de')
+
+  # Event subscription
+  Subscription.create!(subscriber_id: event_subscriber.id, subscribable_id: event.id, subscribable_type: 'Event')
+
+  # User subscription
+  Subscription.create!(subscriber_id: user_subscriber.id, subscribable_id: user.id, subscribable_type: 'User')
+end
