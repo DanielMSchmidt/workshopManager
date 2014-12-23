2829f6b5cc3e485e0c7376a53692cffd710faa3e
Daniel Schmidt <dschmidt@weluse.de>
2014-12-23 18:17:04 +0100

    added controller stuff for the subscription

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -1,5 +1,14 @@
 class SubscriptionController < ApplicationController
   def add
+    AddSubscription.run(params).match do
+      success do |subscription|
+        render "add"
+      end
+
+      failure(:invalid_email) do |error|
+        render "invalid_email"
+      end
+    end
   end
 
   def remove
