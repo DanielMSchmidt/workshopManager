381665a69cd6e4b6bb3c05fd648a0e08796e845c
Daniel Schmidt <dschmidt@weluse.de>
2014-12-29 21:57:24 +0100

    removed mail configs, mailcatcher wont work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,10 +31,4 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
 end
