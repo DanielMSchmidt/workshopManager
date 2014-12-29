8b1a59ea2aa425e1f8e6e438a24dbcfef32707b5
Daniel Schmidt <dschmidt@weluse.de>
2014-12-29 19:52:03 +0100

    changed mails

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,21 +1,18 @@
 class SubscriptionMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "info@workshop-manager.herokuapp.com"
 
   def user(subscription)
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @subscription = subscription
+    mail to: subscription.subscribable.email, subject: "Ein weiterer folgt dir"
   end
 
   def event(subscription)
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @subscription = subscription
+    mail to: subscription.subscribable.user.email, subject: "Neue Anmeldung für #{subscription.subscribable.name}"
   end
 
   def subscriber(subscription)
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @subscription = subscription
+    mail to: subscription.subscriber.email, subject: "Du hast dich erfolgreich angemeldet"
   end
 end
