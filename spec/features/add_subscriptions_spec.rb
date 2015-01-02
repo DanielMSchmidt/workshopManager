874f1a1aad32f9c509806abaa9c61d57a723ecc7
Daniel Schmidt <dschmidt@weluse.de>
2015-01-02 09:58:10 +0100

    full test coverage for the workflow

--- spec/features/add_subscriptions_spec.rb
+++ spec/features/add_subscriptions_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature "AddSubscriptions", :type => :feature do
-  scenario "Unknown users subscribes to an event" do
+  scenario "Unknown subscriber subscribes to an event" do
     # Clean up
     Event.all.destroy_all
     Subscriber.all.destroy_all
@@ -26,15 +26,76 @@ feature "AddSubscriptions", :type => :feature do
     expect(event.subscriptions.first.subscriber).to eq(subscriber)
   end
 
-  scenario "Known users subscribes to an event" do
-    pending
+  scenario "Known subscriber subscribes to an event" do
+    # Clean up
+    Event.all.destroy_all
+    Subscriber.all.destroy_all
+    Subscription.all.destroy_all
+
+    # Setup
+    event = Event.create(name: 'test-event', description: 'test description', user_id: 1, start: Time.now, duration: '2 hours')
+    subscriber = Subscriber.create(email: "test@test.org")
+
+    # Test
+    expect_any_instance_of(Subscription).to receive(:notify).at_least(:once)
+
+    visit subscribe_event_path(event)
+    fill_in "email", :with => "test@test.org"
+    click_button "Anmelden"
+    expect(page).to have_text("Sie wurden erfolgreich angemeldet.")
+
+    # Test model changes (maybe move into unit test)
+    expect(event.subscriptions.count).to eq(1)
+    expect(Subscriber.count).to eq(1)
+    expect(event.subscriptions.first.subscriber).to eq(subscriber)
   end
 
-  scenario "Unknown users subscribes to an user" do
-    pending
+  scenario "Unknown subscriber subscribes to an user" do
+    # Clean up
+    User.all.destroy_all
+    Subscriber.all.destroy_all
+    Subscription.all.destroy_all
+
+    # Setup
+    user = User.create!(email: 'tester@test.de', password: 'testtest', password_confirmation: 'testtest' )
+
+    # Test
+    expect_any_instance_of(Subscription).to receive(:notify).at_least(:once)
+
+    visit subscribe_user_path(user)
+    fill_in "email", :with => "test@test.org"
+    click_button "Anmelden"
+    expect(page).to have_text("Sie wurden erfolgreich angemeldet.")
+
+    # Test model changes (maybe move into unit test)
+    subscriber = Subscriber.where(email: "test@test.org").first
+
+    expect(user.subscriptions.count).to eq(1)
+    expect(Subscriber.count).to eq(1)
+    expect(user.subscriptions.first.subscriber).to eq(subscriber)
   end
 
-  scenario "Known users subscribes to an user" do
-    pending
+  scenario "Known subscriber subscribes to an user" do
+    # Clean up
+    User.all.destroy_all
+    Subscriber.all.destroy_all
+    Subscription.all.destroy_all
+
+    # Setup
+    user = User.create!(email: 'tester@test.de', password: 'testtest', password_confirmation: 'testtest' )
+    subscriber = Subscriber.create(email: "test@test.org")
+
+    # Test
+    expect_any_instance_of(Subscription).to receive(:notify).at_least(:once)
+
+    visit subscribe_user_path(user)
+    fill_in "email", :with => "test@test.org"
+    click_button "Anmelden"
+    expect(page).to have_text("Sie wurden erfolgreich angemeldet.")
+
+    # Test model changes (maybe move into unit test)
+    expect(user.subscriptions.count).to eq(1)
+    expect(Subscriber.count).to eq(1)
+    expect(user.subscriptions.first.subscriber).to eq(subscriber)
   end
 end
