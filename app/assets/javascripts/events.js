df8bf4e0d00340d25f844f51c31acfafbb61da03
Daniel Schmidt <dschmidt@weluse.de>
2015-01-05 16:57:06 +0100

    enough ruby for today :D

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,7 +1,7 @@
 $('.js-unsubscribe-event-form').on('submit', function (e){
-  # Prevent form from being submit
-  # Get the right token and put it into the path
-  # Do a get request, while adding a spinner
-    # On fail: Cancel spinner and alert the error, clear the form
-    # On success: Cancel spinner and render a thank you page like message, which replaces the form
+  // Prevent form from being submit
+  // Get the right token and put it into the path
+  // Do a get request, while adding a spinner
+    // On fail: Cancel spinner and alert the error, clear the form
+    // On success: Cancel spinner and render a thank you page like message, which replaces the form
 });
