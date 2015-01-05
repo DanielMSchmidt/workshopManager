700f7267228aff8d9dc9e7a0c28c84f0a69a5df1
Daniel Schmidt <dschmidt@weluse.de>
2015-01-05 16:13:39 +0100

    added tests for exception cases

--- spec/features/remove_subscriptions_spec.rb
+++ spec/features/remove_subscriptions_spec.rb
@@ -21,7 +21,7 @@ feature "RemoveSubscriptions", :type => :feature do
     # Test
     visit subscription_remove_path({unsubscribe_token: token})
 
-    # Test model changes (maybe move into unit test)
+    # Test model changes
     expect(event.subscriptions.count).to eq(0)
     expect(subscriber.subscriptions.count).to eq(0)
   end
@@ -44,7 +44,75 @@ feature "RemoveSubscriptions", :type => :feature do
     # Test
     visit subscription_remove_path({unsubscribe_token: token})
 
-    # Test model changes (maybe move into unit test)
+    # Test model changes
+    expect(user.subscriptions.count).to eq(0)
+    expect(subscriber.subscriptions.count).to eq(0)
+  end
+
+  scenario "Unsubscribe to an event with completely invalid token" do
+    # Clean up
+    Event.all.destroy_all
+    User.all.destroy_all
+    Subscriber.all.destroy_all
+    Subscription.all.destroy_all
+
+    # Setup
+    subscriber = Subscriber.create(email: "test@test.org")
+    user = User.create!(email: 'tester@test.de', password: 'testtest', password_confirmation: 'testtest' )
+    token = "completelyuselesstoken"
+    Subscription.create(subscriber_id: subscriber.id, subscribable_type: 'User', subscribable_id: user.id)
+    expect(user.subscriptions.count).to eq(1)
+    expect(subscriber.subscriptions.count).to eq(1)
+
+    # Test
+    visit subscription_remove_path({unsubscribe_token: token})
+
+    # Test model changes
+    expect(user.subscriptions.count).to eq(1)
+    expect(subscriber.subscriptions.count).to eq(1)
+  end
+
+  scenario "Unsubscribe to an event with unknown subscriber" do
+    # Clean up
+    Event.all.destroy_all
+    User.all.destroy_all
+    Subscriber.all.destroy_all
+    Subscription.all.destroy_all
+
+    # Setup
+    subscriber = Subscriber.create(email: "test@test.org")
+    user = User.create!(email: 'tester@test.de', password: 'testtest', password_confirmation: 'testtest' )
+    token = Base64.encode64("User,#{user.id},unknown@test.org")
+    Subscription.create(subscriber_id: subscriber.id, subscribable_type: 'User', subscribable_id: user.id)
+    expect(user.subscriptions.count).to eq(1)
+    expect(subscriber.subscriptions.count).to eq(1)
+
+    # Test
+    visit subscription_remove_path({unsubscribe_token: token})
+
+    # Test model changes
+    expect(user.subscriptions.count).to eq(1)
+    expect(subscriber.subscriptions.count).to eq(1)
+  end
+
+  scenario "Unsubscribe where no subscription exists" do
+    # Clean up
+    Event.all.destroy_all
+    User.all.destroy_all
+    Subscriber.all.destroy_all
+    Subscription.all.destroy_all
+
+    # Setup
+    subscriber = Subscriber.create(email: "test@test.org")
+    user = User.create!(email: 'tester@test.de', password: 'testtest', password_confirmation: 'testtest' )
+    token = Base64.encode64("User,#{user.id},test@test.org")
+    expect(user.subscriptions.count).to eq(0)
+    expect(subscriber.subscriptions.count).to eq(0)
+
+    # Test
+    visit subscription_remove_path({unsubscribe_token: token})
+
+    # Test model changes
     expect(user.subscriptions.count).to eq(0)
     expect(subscriber.subscriptions.count).to eq(0)
   end
