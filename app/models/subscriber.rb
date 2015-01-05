6f98406fb8d7e9345db71afb554d136c22b0ec8f
Daniel Schmidt <dschmidt@weluse.de>
2015-01-05 17:44:01 +0100

    added call of subscriptionMailer.notify call

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -3,5 +3,9 @@ class Subscriber < ActiveRecord::Base
 
   def notify(subscribable, message)
     # TODO: send message from subscribable to self
+    subscribable_email = 'Get the email from subscribable'
+    subject = 'Get subject from subscribable'
+
+    SubscriberMailer.notify(subscribable_email, self.email, subject, message).deliver
   end
 end
