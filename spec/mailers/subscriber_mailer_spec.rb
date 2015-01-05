6f0291916c7ddea5ae7397d2521b3d8e8e35c580
Daniel Schmidt <dschmidt@weluse.de>
2015-01-05 17:48:31 +0100

    wrote subscriber mail spec

--- spec/mailers/subscriber_mailer_spec.rb
+++ spec/mailers/subscriber_mailer_spec.rb
@@ -2,20 +2,17 @@ require "rails_helper"
 
 RSpec.describe SubscriberMailer, :type => :mailer do
   describe "notify" do
-    let(:mail) { SubscriberMailer.notify }
+
+    let(:mail) { SubscriberMailer.notify('from@test.org', 'to@test.org', 'test-subject', 'test-message') }
 
     it "renders the headers" do
-      # expect(mail.subject).to eq("Notify")
-      # expect(mail.to).to eq(["to@example.org"])
-      # expect(mail.from).to eq(["from@example.com"])
-
-      pending
+      expect(mail.subject).to eq("test-subject")
+      expect(mail.to).to eq(["to@test.org"])
+      expect(mail.from).to eq(["from@test.org"])
     end
 
     it "renders the body" do
-      # expect(mail.body.encoded).to match("Hi")
-
-      pending
+      expect(mail.body.encoded).to match("test-message")
     end
   end
 
