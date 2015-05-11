7782223863055abf618e5882448ffa6008441e34
Daniel Schmidt <dschmidt@weluse.de>
2015-05-11 12:30:20 +0200

    added todos

--- app/assets/javascripts/components/subscription_form.js.jsx
+++ app/assets/javascripts/components/subscription_form.js.jsx
@@ -6,9 +6,17 @@ var SubscriptionForm = React.createClass({
     subscribableId: React.PropTypes.number.isRequired
   },
 
+  componentWillMount: function() {
+    // TODO: Check if the user is already registerd for this subscribable (add this kind of thing to propType or load & refetch it)
+  },
+
+  handleSubmit: function() {
+    // TODO: subscribe / unsubscribe user (see events.js)
+  },
+
   render: function() {
     return (
-      <form accept-charset="UTF-8" method="POST">
+      <form accept-charset="UTF-8" method="POST" onSubmit={this.handleSubmit}>
         <div className="form-group">
           <label htmlFor="email">E-Mail Adresse</label><br />
           <input name="email" type="email" placeholder="Bitte E-Mail Adresse eingeben" className="form-control" />
